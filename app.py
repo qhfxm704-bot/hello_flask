1f69773b018d882ee0c5291e22327e357c481404
Leejeonghak <qhfxm704@gmail.com>
2026-05-06 10:17:05 +0900

    딕셔너리 create / read 구현

--- app.py
+++ app.py
@@ -1,64 +1,27 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-messages = []
+
+messages = [
+    {"skill": "Python"},
+    {"skill": "HTML"},
+    {"skill": "SQL"}
+    ]
 
 @app.route('/')
 def home():
-    skills = [
-        "Python", "HTML", "SQL", "Bash"
-    ]
-    return render_template('index.html', data=skills, messages=messages)
+    return render_template('index.html', messages=messages)
 
 @app.route('/send', methods=['POST'])
 def send():
-    skill = request.form.get('skill')
-    level = request.form.get('level')
-    status = request.form.get('status')
+    skill = request.form.get('skill', '').strip()
 
-    messages.append(skill + " / " + level + " / " + status)
-
-    return redirect('/')
-
-@app.route('/delete', methods=['POST'])
-def delete():
-    index = int(request.form['index'])
-    messages.pop(index)
-    return redirect('/')
-
-@app.route('/delete_all', methods=['post'])
-def delete_all():
-    messages.clear()
-    return redirect('/')
-
-@app.route('/delete_selected', methods=['post'])
-def delete_selected():
-    indexes = request.form.getlist('delete_indexes')
-    indexes = [int(index) for index in indexes]
-    indexes.sort(reverse=True)
-
-    for index in indexes:
-        messages.pop(index)
-
-    return redirect('/')
-
-@app.route('/edit/<int:index>')
-def edit(index):
-    return render_template('edit.html', index=index, item=messages[index])
-
-@app.route('/update', methods=['POST'])
-def update():
-    index = int(request.form['index'])
-    value = request.form.get('value')
-
-    if value.strip() == "":
-        return redirect(f'/edit/{index}')
-
-    if value == messages[index]:
+    if not skill:
         return redirect('/')
-
-    messages[index] = value
+    
+    messages.append({"skill": skill})
+    
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
